8943dfc4154ccf9d1442915db0216a96c9c15231
cykzht <54177575+cykzht@users.noreply.github.com>
2023-02-25 10:37:34 +0800

    2.25
    2.0版本，加入历史成绩查询。

--- README.md
+++ README.md
@@ -1,27 +1,20 @@
 # Season-zhixue
 
-### 该项目已停止更新
-
-由于这一届的高三学生毕业，故网站不再进行技术更新，可能存在一些未知的bug没有修复，如遇500错误请稍后重试。
-
-[网站](https://zhixue.g2022cyk.top:6081/ "退学网")还会继续运营，直到2023年5月20日域名到期，如有想运营网站的同学请通过微信联系我。
-![1657788094689](static/images/1657788094689.png)
-
 ### 介绍
-
 四季学——退学网，可以绕过某学网的限制，获取到自己真实的成绩。
 
 正如它的名字一样，这是一个会变换四季的程序，每次启动的时候都会重新选择一个季节作为主题季节。
 
-### 安装教程
+![show](static\images\show.jpg)
 
+### 安装教程
 #### 环境配置
 
 ```python
 pip3 install flask
-pip3 install zhixuewang
-pip3 install flask_limiter
+pip3 install zhixuewang==1.1.4
 ```
+
 
 ### 使用说明
 
@@ -31,47 +24,61 @@ pip3 install flask_limiter
 
 3、输入某学网的账号和密码即可查询最新一次的考试成绩。
 
+
 ### TODO
 
-- [X] 记住密码
-  （貌似不起作用，在Javascript里，使用cookie）
-- [X] 排名查询
-  （找不到接口）
-- [ ] 历史成绩查询
-  （没什么大用，主要是接口调用太复杂，咕了）
+- [x] 记住密码
+（貌似不起作用，在Javascript里，使用cookie）
+- [x] 排名查询
+（找不到接口）
+- [x] 历史成绩查询
+（没什么大用，主要是接口调用太复杂，咕了）
+
 
 ### 修改建议
 
 1. 如果要使用cookie或者session请先配置一下密钥。
 
-```python
-app.config["SECRET_KEY"] = 'abc'  # 配置密钥
-```
+   ```python
+   app.config["SECRET_KEY"] = 'abc'  # 配置密钥
+   ```
 
-2. 如需控制网站访问量可以设置Limiter。
+2. 如需控制网站访问量可以设置flask-Limiter。
 
-```python
-limiter = Limiter(  # 限制访问量
-    app,
-    key_func=get_remote_address,  # 根据访问者的IP记录访问次数
-    default_limits=["200 per day", "20 per minute"]  # 默认限制，一分钟最多访问20次
-)
-```
+   - 安装：
+
+     ```bash
+     pip3 install flask_limiter==2.20
+     ```
+
+   - 使用：
+
+     ```python
+     from flask_limiter import Limiter
+     from flask_limiter.util import get_remote_address
+     
+     limiter = Limiter(  # 限制访问量
+         app,
+         key_func=get_remote_address,  # 根据访问者的IP记录访问次数
+         default_limits=["200 per day", "20 per minute"]  # 默认限制，一分钟最多访问20次
+     )
+     ```
 
 2. 如果你的网站使用了Nginx代理，请为Limiter加入以下代码。
 
-```python
-def limit_key_func():
-    return str(request.headers.get("X-Forwarded-For", '127.0.0.1'))
-limiter = Limiter(  # 限制访问量
-    app,
-    key_func=limit_key_func,  # 根据访问者的IP记录访问次数
-    default_limits=["200 per day", "20 per minute"]  # 默认限制，一分钟最多访问20次
-)
-```
+   ```python
+   def limit_key_func():
+       return str(request.headers.get("X-Forwarded-For", '127.0.0.1'))
+   limiter = Limiter(  # 限制访问量
+       app,
+       key_func=limit_key_func,  # 根据访问者的IP记录访问次数
+       default_limits=["200 per day", "20 per minute"]  # 默认限制，一分钟最多访问20次
+   )
+   ```
 
-3. 错误页面在[error.html](templates/error.html)里。
+2. 错误页面在[error.html](templates/error.html)里。
 
 ### 参与贡献
 
 本项目的诞生离不开[zhixuewang](https://github.com/anwenhu/zhixuewang-python)、[layui](https://gitee.com/sentsin/layui)、[Flask](https://github.com/pallets/flask)。
+
--- app.py
+++ app.py
@@ -1,88 +1,129 @@
 from flask import *
 import random
 from zhixuewang import login_student
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
+import base64
+import requests
+
 app = Flask(__name__, static_url_path='')
 app.config["SECRET_KEY"] = 'abc'  # 配置密钥
 
-limiter = Limiter(  # 限制访问量
-    app,
-    key_func=get_remote_address,  # 根据访问者的IP记录访问次数
-    default_limits=["200 per day", "20 per minute"]  # 默认限制，一分钟最多访问20次
-)
 
 # 错误返回
-
-
 @app.errorhandler(404)
-def miss(e):
-    return render_template("error.html", msg='404 Not Found.')
-
-
 @app.errorhandler(500)
-def error(e):
-    return render_template("error.html", msg='500 IIS Error.')
-
-
 @app.errorhandler(429)
 def error(e):
-    return render_template("error.html", msg='429 Too Frequently.')
+    if (e.code == 404):
+        return render_template("error.html", msg='404错误，页面不存在。', code=404)
+    elif (e.code == 500):
+        return render_template("error.html", msg='500服务器内部错误。', code=500)
+    elif (e.code == 429):
+        return render_template("error.html", msg='429访问过于频繁，请稍后再试。', code=429)
+    else:
+        return render_template("error.html", msg='500服务器内部错误。', code=500)
 
 
-# 主界面
-@app.route('/', methods=['GET', 'POST'])
-@limiter.exempt
-def index():
-    # 随机选取季节
+def get_season():
+    '''
+    随机选取季节
+    slog: 季节名
+    pic: 背景图片
+    color: 主题颜色
+    '''
     ran = random.randint(1, 4)
-    if(ran == 1):
+    if (ran == 1):
         slog = '退学网——秋'
         color = '#e5bc00'
-        pic = 'images/web_login_bg1.jpg'
-    elif(ran == 2):
+        pic = 'images/web_login_bg1.webp'
+    elif (ran == 2):
         slog = '退学网——夏'
         color = '#50e45c'
-        pic = 'images/web_login_bg2.jpg'
-    elif(ran == 3):
+        pic = 'images/web_login_bg2.webp'
+    elif (ran == 3):
         slog = '退学网——春'
         color = '#f1beea'
-        pic = 'images/web_login_bg3.jpg'
-    elif(ran == 4):
+        pic = 'images/web_login_bg3.webp'
+    elif (ran == 4):
         slog = '退学网——冬'
         color = '#27A9E3'
-        pic = 'images/web_login_bg4.jpg'
+        pic = 'images/web_login_bg4.webp'
+    return slog, pic, color
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    season = get_season()
+    session['season'] = season
+    if (request.method == 'POST'):
+        username = request.form.get("username")
+        password = request.form.get("password")
+        session['username'] = username
+        session['password'] = password
+        return redirect(url_for('login'))
+    if (session.get('username')):
+        return redirect(url_for('login'))
+    else:
+        return render_template('index.html', season=season)
+
+
+@app.route('/mark', methods=['GET', 'POST'])
+def login():
+    season = session.get('season')
+    if (not season):
+        season = get_season()
     # 处理登录请求
-    if(request.method == 'POST' and request.values.get("username")):
-        data = []
-        c = []
-        d = []
-        username = request.values.get("username")
-        password = request.values.get("password")
-        exams = 0
+    if (not session.get('username')):
+        return redirect(url_for('index'))
+    else:
+        username = session.get('username')
+        password = session.get('password')
+        exam = request.args.get('exam')
         try:
             zxw = login_student(username, password)
             exams = zxw.get_exams()
-        except:
-            return render_template("index.html", slog=slog, pic=pic, color=color, msg='账号或密码错误')
+        except Exception as e:
+            session.clear()
+            return render_template("index.html", season=season, message=e)
         if len(exams) != 0:
-            a = zxw.get_self_mark()
-            a = str(a)
-            a = a.split("\n")
-            b = a[0]
-            del a[0]
-            for i in a:
-                if("分数" in i):
-                    c.append(i)
+            data = []
+            examlist = []
+            for i in exams:
+                data.append(i.id)
+                examlist.append((i.id, i.name))
+            try:
+                pic = requests.get(zxw.avatar).content
+                avatar = base64.b64encode(pic)
+                avatar = 'data:image/png;base64,'+str(avatar, 'utf-8')
+            except:
+                avatar = 'images/avatar_default_student_200_m_3.png'
+            if (exam):
+                if (exam in data):
+                    try:
+                        a = zxw.get_self_mark(exam)
+                    except Exception as e:
+                        return render_template("examlist.html", data=examlist, season=season, avatar=avatar, message="本场考试未扫描")
+                    msg = a.person.name+'-'+a.exam.name
+                    data = []
+                    for i in a:
+                        if (i.class_rank):
+                            data.append(
+                                (i.subject.name, i.score, i.class_rank))
+                        else:
+                            data.append((i.subject.name, i.score))
+                    return render_template("zhixue.html", data=data, season=season, title=msg, avatar=avatar)
                 else:
-                    d.append(i)
-            for i in range(len(c)):
-                data.append((d[i].replace(':', ''), c[i].replace('分数: ', '')))
-            return render_template("zhixue.html", data=data, title=b, pic=pic, color=color)
+                    return render_template("examlist.html", data=examlist, season=season, avatar=avatar, message="该考试不可查询")
+            else:
+                return render_template("examlist.html", data=examlist, season=season, avatar=avatar)
         else:
-            return render_template("index.html", slog=slog, pic=pic, color=color, msg='未检测到考试')
-    return render_template('index.html', slog=slog, pic=pic, color=color)
+            return render_template("index.html", season=season, message='未检测到考试')
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='127.0.0.1', port=6082, debug=True)
